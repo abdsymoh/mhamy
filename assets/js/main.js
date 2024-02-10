3baf0b39a227d45f0ac8cffb1c77beecd57715ae
John DoeAbdullah <abdsydev@gmail.com>
2024-02-10 04:56:19 +0200

    add task box

---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+document.getElementById("addBtn").addEventListener("click", function () {
+  document.getElementById("addTaskBox").style.display = "flex";
+});
+
+document.getElementById("closeTaskBox").addEventListener("click", function () {
+  document.getElementById("addTaskBox").style.display = "none";
+});
