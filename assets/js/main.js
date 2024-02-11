73d7a5abddee5d35de1484073554912e3b24e261
John DoeAbdullah <abdsydev@gmail.com>
2024-02-11 18:50:13 +0200

    add edit task

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,8 @@
 // Tasks
 let tasks = [
   {
-    title: "تجربة المهمة",
-    date: "10/02/2023",
+    title: "المهمة الأولى",
+    date: "9/2/2023",
     isDone: false,
   },
 ];
@@ -20,7 +20,7 @@ function fillTasksOnThePage() {
         </div>
         <div class="taskActions">
           <button class="btn deleteBtn" id="deleteBtn" onclick = "deleteTask(${index})"><i class="fa-solid fa-trash-can"></i></button>
-          <button class="btn editBtn" id="editBtn"><i class="fa-solid fa-pencil"></i></button>
+          <button class="btn editBtn" id="editBtn" onclick="editTask(${index})"><i class="fa-solid fa-pencil"></i></button>
           <button class="btn checkBtn" id="checkBtn"><i class="fa-solid fa-check"></i></button>
         </div>
       </div>
@@ -59,8 +59,10 @@ function deleteTask(index) {
   document.getElementById("confirmDeleteTask").style.display = "flex";
   document.getElementById("confirmDeleteTask").innerHTML = `
   <p>هل ترغب في حذف: ${task.title}</p>
-  <button id="yesBtn" class="btn yesBtn"><i class="fa-solid fa-trash-can"></i></button>
-  <button id="noBtn" class="btn noBtn"><i class="fa-solid fa-xmark"></i></button>
+  <span>
+    <button id="yesBtn" class="btn yesBtn"><i class="fa-solid fa-trash-can"></i></button>
+    <button id="noBtn" class="btn noBtn"><i class="fa-solid fa-xmark"></i></button>
+  </span>
   `;
   document.getElementById("yesBtn").addEventListener("click", function () {
     tasks.splice(index, 1);
@@ -70,6 +72,18 @@ function deleteTask(index) {
   document.getElementById("noBtn").addEventListener("click", function () {
     document.getElementById("confirmDeleteTask").style.display = "none";
   });
-  // confirm("هل أنت متأكد من حذف: " + task.title);
 }
-// console.log(index);
+
+// Edit Task
+
+function editTask(index) {
+  document.getElementById("editTaskBox").style.display = "flex";
+  document.getElementById("editTask").addEventListener("click", function () {
+    let newTaskTitle = document.getElementById("NewTaskInput").value;
+    let task = tasks[index];
+    task.title = newTaskTitle;
+    fillTasksOnThePage();
+    document.getElementById("taskInput").value = "";
+    document.getElementById("editTaskBox").style.display = "none";
+  });
+}
