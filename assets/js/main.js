d0f60f2e0554b6a4487f802c8702ab65b1b676f6
John DoeAbdullah <abdsydev@gmail.com>
2024-02-11 17:07:39 +0200

    add delete tasks

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,22 +8,25 @@ let tasks = [
 ];
 function fillTasksOnThePage() {
   document.getElementById("tasks").innerHTML = "";
+  let index = 0;
   for (let task of tasks) {
     document.getElementById("tasks").innerHTML += `
+    <!-- end confirmDeleteTask -->
       <!-- task -->
-      <div class="task">
+      <div class="task" id="task">
         <div class="taskContent">
           <h1 class="taskTitle">${task.title}</h1>
           <span class="date"><i class="fa-solid fa-calendar-days"></i> ${task.date}</span>
         </div>
         <div class="taskActions">
-          <button class="btn deleteBtn" id="deleteBtn"><i class="fa-solid fa-trash-can"></i></button>
+          <button class="btn deleteBtn" id="deleteBtn" onclick = "deleteTask(${index})"><i class="fa-solid fa-trash-can"></i></button>
           <button class="btn editBtn" id="editBtn"><i class="fa-solid fa-pencil"></i></button>
           <button class="btn checkBtn" id="checkBtn"><i class="fa-solid fa-check"></i></button>
         </div>
       </div>
       <!-- end task -->
     `;
+    index++;
   }
 }
 
@@ -48,3 +51,25 @@ document.getElementById("addTask").addEventListener("click", function () {
   fillTasksOnThePage();
   document.getElementById("addTaskBox").style.display = "none";
 });
+
+// Delete Task
+
+function deleteTask(index) {
+  let task = tasks[index];
+  document.getElementById("confirmDeleteTask").style.display = "flex";
+  document.getElementById("confirmDeleteTask").innerHTML = `
+  <p>هل ترغب في حذف: ${task.title}</p>
+  <button id="yesBtn" class="btn yesBtn"><i class="fa-solid fa-trash-can"></i></button>
+  <button id="noBtn" class="btn noBtn"><i class="fa-solid fa-xmark"></i></button>
+  `;
+  document.getElementById("yesBtn").addEventListener("click", function () {
+    tasks.splice(index, 1);
+    document.getElementById("confirmDeleteTask").style.display = "none";
+    fillTasksOnThePage();
+  });
+  document.getElementById("noBtn").addEventListener("click", function () {
+    document.getElementById("confirmDeleteTask").style.display = "none";
+  });
+  // confirm("هل أنت متأكد من حذف: " + task.title);
+}
+// console.log(index);
